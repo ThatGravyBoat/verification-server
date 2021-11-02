8b0c4128916e2202db0b1b14052c5ad2f8303a46
ThatGravyBoat <thatgravyboat@gmail.com>
2021-11-02 16:27:29 -0230

    Stop querying these servers will most likely all be hidden to the public and should never need to be queried also multimc are weird so screw them too

--- src/main/java/com/resourcefulbees/server/Server.java
+++ src/main/java/com/resourcefulbees/server/Server.java
@@ -1,5 +1,6 @@
 package com.resourcefulbees.server;
 
+import com.google.common.collect.Sets;
 import com.google.gson.JsonSyntaxException;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -8,7 +9,10 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerPreLoginEvent;
 import net.minestom.server.event.server.ServerListPingEvent;
+import net.minestom.server.event.trait.CancellableEvent;
 import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.query.event.BasicQueryEvent;
+import net.minestom.server.extras.query.event.FullQueryEvent;
 import net.minestom.server.ping.ResponseData;
 
 import java.io.BufferedReader;
@@ -16,13 +20,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.URL;
 import java.util.Optional;
+import java.util.Set;
 
 public class Server {
 
-    public static final MinecraftServer VERIFICATION_SERVER = MinecraftServer.init();
-    public static final String UUID_REPLACE_STRING = "${uuid}";
+    private static final MinecraftServer VERIFICATION_SERVER = MinecraftServer.init();
+    private static final String UUID_REPLACE_STRING = "${uuid}";
+    private static final Set<String> NAME_MC_ADDRESSES = Sets.newHashSet("2606:4700:20::681b:1272", "104.27.18.114", "51.222.110.150");
 
     public static void main(String[] args) {
         Config.read();
@@ -34,13 +42,26 @@ public class Server {
 
         GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
 
+        eventHandler.addListener(FullQueryEvent.class, Server::cancel);
+        eventHandler.addListener(BasicQueryEvent.class, Server::cancel);
+
         eventHandler.addListener(AsyncPlayerPreLoginEvent.class, Server::onLogin);
         eventHandler.addListener(ServerListPingEvent.class, Server::onPing);
 
         VERIFICATION_SERVER.start("0.0.0.0", Integer.parseInt(args[0]));
     }
 
+    private static <T extends CancellableEvent> void cancel(T event) {
+        event.setCancelled(true);
+    }
+
     private static void onPing(ServerListPingEvent event) {
+        if (event.getConnection() != null) {
+            SocketAddress socket = event.getConnection().getRemoteAddress();
+            if (socket instanceof InetSocketAddress address && NAME_MC_ADDRESSES.contains(address.getAddress().getHostAddress())) {
+                event.setCancelled(true);
+            }
+        }
         ResponseData responseData = event.getResponseData();
         responseData.setPlayersHidden(true);
         Config.getServerDescription().ifPresent(desc -> responseData.setDescription(Component.text(desc)));
